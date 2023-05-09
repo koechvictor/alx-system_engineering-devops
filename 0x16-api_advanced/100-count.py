813d6619cf9a10a8db3836b39ff6291edad273b2
victor koech <kipkiruiv408@gmail.com>
2023-05-09 15:26:43 +0000

    update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -9,7 +9,16 @@ def sort_histogram(histogram={}):
     '''
     histogram = list(filter(lambda kv: kv[1], histogram))
     histogram_dict = {}
-
+    for item in histogram:
+        if item[0] in histogram_dict:
+            histogram_dict[item[0]] += item[1]
+        else:
+            histogram_dict[item[0]] = item[1]
+    histogram = list(histogram_dict.items())
+    histogram.sort(
+        key=lambda kv: kv[0],
+        reverse=False
+    )
     histogram.sort(
         key=lambda kv: kv[1],
         reverse=True
@@ -18,6 +27,10 @@ def sort_histogram(histogram={}):
         lambda kv: '{}: {}'.format(kv[0], kv[1]),
         histogram
     )))
+    if res_str:
+        print(res_str)
+
+
 def count_words(subreddit, word_list, histogram=[], n=0, after=None):
     '''Counts the number of times each word in a given wordlist
     occurs in a given subreddit.
@@ -31,6 +44,7 @@ def count_words(subreddit, word_list, histogram=[], n=0, after=None):
             'Safari/537.36',
             'Edg/97.0.1072.62'
         ])
+    }
     sort = 'hot'
     limit = 30
     res = requests.get(
@@ -44,7 +58,7 @@ def count_words(subreddit, word_list, histogram=[], n=0, after=None):
         ),
         headers=api_headers,
         allow_redirects=False
-    )} 
+    )
     if not histogram:
         word_list = list(map(lambda word: word.lower(), word_list))
         histogram = list(map(lambda word: (word, 0), word_list))
@@ -58,4 +72,16 @@ def count_words(subreddit, word_list, histogram=[], n=0, after=None):
                 titles
             )))),
             histogram
-        )) 
+        ))
+        if len(posts) >= limit and data['after']:
+            count_words(
+                subreddit,
+                word_list,
+                histogram,
+                n + len(posts),
+                data['after']
+            )
+        else:
+            sort_histogram(histogram)
+    else:
+        return
