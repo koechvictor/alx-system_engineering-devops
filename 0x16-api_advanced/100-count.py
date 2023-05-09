5ef23591038968e0bf98ca1bb04ffd01c595cdd3
victor koech <kipkiruiv408@gmail.com>
2023-05-09 15:22:14 +0000

    update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -4,6 +4,20 @@
 import requests
 
 
+def sort_histogram(histogram={}):
+    '''Sorts and prints the given histogram.
+    '''
+    histogram = list(filter(lambda kv: kv[1], histogram))
+    histogram_dict = {}
+
+    histogram.sort(
+        key=lambda kv: kv[1],
+        reverse=True
+    )
+    res_str = '\n'.join(list(map(
+        lambda kv: '{}: {}'.format(kv[0], kv[1]),
+        histogram
+    )))
 def count_words(subreddit, word_list, histogram=[], n=0, after=None):
     '''Counts the number of times each word in a given wordlist
     occurs in a given subreddit.
