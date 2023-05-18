68e64bbf6ee092f0f11d36025a4361316aabe492
victor koech <kipkiruiv408@gmail.com>
2023-05-18 09:10:14 +0000

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,5 @@
-#testing how well web server setup featuring Nginx is doing under pressure
+#
+#esting how well web server setup featuring Nginx is doing under pressure
 
 exec { 'fix_error':
   command => "/bin/sed -i 's/15/4096/g' /etc/default/nginx; ; sudo service nginx restart",
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,9 @@
 #fix os error change config
 
-exec { 'config':
-  command  => "/bin/sed -i \'s/holberton/#holberton/\' /\
-etc/security/limits.conf",
+exec { 'change-os-configuration-for-holberton-user':
+  command => "bash -c \"sed -iE 's/^holberton hard nofile \
+5/holberton hard nofile 88888/' /etc/security/limits.conf; \
+sed -iE 's/^holberton soft nofile \
+4/holberton soft nofile 88888/' /etc/security/limits.conf\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
