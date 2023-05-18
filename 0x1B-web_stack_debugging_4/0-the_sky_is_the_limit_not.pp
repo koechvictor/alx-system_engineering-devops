ac82a5644214524dfdd5074b32bf7976d6af6f96
victor koech <kipkiruiv408@gmail.com>
2023-05-18 09:12:02 +0000

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,7 @@
-#
-#esting how well web server setup featuring Nginx is doing under pressure
+#testing how well web server setup featuring Nginx is doing under pressure
 
-exec { 'fix_error':
-  command => "/bin/sed -i 's/15/4096/g' /etc/default/nginx; ; sudo service nginx restart",
+exec { 'fix--for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
